CyanogenMod/android_hardware_qcom_display

hwc/copybit: Handle "R/B swap" for RGBA and RGBX color formats.
1.Framework notifies HAL with "HWC_FORMAT_RB_SWAP" layer flag to handle
a limitation where R and B components were swapped in Rendering phase.
2.Add "R/B swap" in hwc query to enable framework to query for support
in display HAL, at run-time.

Change-Id: I3b44d15b51b4f24939048fee9d1bac2b9009c97c

libgralloc/gralloc_priv.h
@@ -104,6 +104,7 @@ enum {
     HAL_PIXEL_FORMAT_YCbCr_444_SP           = 0x10F,
     HAL_PIXEL_FORMAT_YCrCb_444_SP           = 0x110,
     HAL_PIXEL_FORMAT_YCrCb_422_I            = 0x111,
+    HAL_PIXEL_FORMAT_BGRX_8888              = 0x112,
     HAL_PIXEL_FORMAT_INTERLACE              = 0x180,
 
 };

chirayudesai/android_hardware_qcom_display-caf

new/libgralloc/gralloc_priv.h
@@ -106,6 +106,7 @@ enum {
     HAL_PIXEL_FORMAT_YCrCb_444_SP           = 0x110,
     HAL_PIXEL_FORMAT_YCrCb_422_I            = 0x111,
     HAL_PIXEL_FORMAT_NV21_ZSL               = 0x112,
+    HAL_PIXEL_FORMAT_BGRX_8888              = 0x113,
     HAL_PIXEL_FORMAT_INTERLACE              = 0x180,
 
 };